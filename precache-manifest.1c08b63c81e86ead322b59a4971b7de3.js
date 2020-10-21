509b47d405b1894e0746c11900de62d097fddcc0
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-22 00:12:12 +0300

    Updates

---
+++ precache-manifest.1c08b63c81e86ead322b59a4971b7de3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e7c98162829727852c66ef3bc7e1fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d0ba327ba98008e48eb",
+    "url": "./static/css/main.83704e11.chunk.css"
+  },
+  {
+    "revision": "236e0f5a66d02ca9f43a",
+    "url": "./static/js/2.956c6d12.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.956c6d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d0ba327ba98008e48eb",
+    "url": "./static/js/main.988e0845.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  },
+  {
+    "revision": "2025d9fb8bba12bedb7f6f1c7dc65b75",
+    "url": "./static/media/completed.2025d9fb.svg"
+  },
+  {
+    "revision": "749645a0e7e43e3d1b3025b646a95ffc",
+    "url": "./static/media/not_completed.749645a0.svg"
+  }
+]);
